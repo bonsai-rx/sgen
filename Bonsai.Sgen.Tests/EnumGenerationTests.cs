69f63161e4dc03066ab6f7b4ea701415e93fbaa2
glopesdev <glopesdev@users.noreply.github.com>
2023-12-28 09:12:00 +0000

    Add integer and string enum generation tests

---
+++ Bonsai.Sgen.Tests/EnumGenerationTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using NJsonSchema;
+
+namespace Bonsai.Sgen.Tests
+{
+    [TestClass]
+    public class EnumGenerationTests
+    {
+        public class Foo
+        {
+            public Bar Bar { get; set; }
+
+            [JsonConverter(typeof(StringEnumConverter))]
+            public Bar Bar2 { get; set; }
+        }
+
+        public enum Bar
+        {
+            A = 0,
+            B = 5,
+            C = 6
+        }
+
+        [TestMethod]
+        public void GenerateIntegerEnum_SerializerAnnotationsUseIntegerValues()
+        {
+            var schema = JsonSchema.FromType<Foo>();
+            var generator = TestHelper.CreateGenerator(schema);
+            var code = generator.GenerateFile();
+            Assert.IsTrue(code.Contains("EnumMemberAttribute(Value=\"0\")"));
+            Assert.IsTrue(code.Contains("YamlMemberAttribute(Alias=\"0\")"));
+        }
+
+        [TestMethod]
+        public void GenerateStringEnum_SerializerAnnotationsUseStringValues()
+        {
+            var schema = JsonSchema.FromType<Foo>();
+            var generator = TestHelper.CreateGenerator(schema);
+            var code = generator.GenerateFile();
+            Assert.IsTrue(code.Contains("EnumMemberAttribute(Value=\"A\")"));
+            Assert.IsTrue(code.Contains("YamlMemberAttribute(Alias=\"A\")"));
+        }
+    }
+}
