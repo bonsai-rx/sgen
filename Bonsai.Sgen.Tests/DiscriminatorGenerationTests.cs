eec64109dd3528de10fae518c1eb4292fd2102bc
glopesdev <glopesdev@users.noreply.github.com>
2024-01-15 10:43:20 +0000

    Add test for oneOf inheritance

--- Bonsai.Sgen.Tests/DiscriminatorGenerationTests.cs
+++ Bonsai.Sgen.Tests/DiscriminatorGenerationTests.cs
@@ -80,5 +80,103 @@ namespace Bonsai.Sgen.Tests
             Assert.IsTrue(code.Contains("[YamlDiscriminator(\"discriminator\")]"));
             CompilerTestHelper.CompileFromSource(code);
         }
+
+        [TestMethod]
+        public async Task GenerateFromOneOfDiscriminatorSchema_SerializerAnnotationsDeclareKnownTypes()
+        {
+            var schema = await JsonSchema.FromJsonAsync(@"
+{
+    ""$schema"": ""http://json-schema.org/draft-04/schema#"",
+    ""type"": ""object"",
+    ""title"": ""Container"",
+    ""properties"": {
+      ""Animal"": {
+        ""oneOf"": [
+          {
+            ""$ref"": ""#/definitions/Dog""
+          },
+          {
+            ""$ref"": ""#/definitions/Cat""
+          },
+          {
+            ""type"": ""null""
+          }
+        ]
+      }
+    },
+    ""definitions"": {
+      ""Dog"": {
+        ""type"": ""object"",
+        ""properties"": {
+          ""kind"": {
+            ""type"": ""string"",
+            ""enum"": [ ""Dog"" ]
+          },
+          ""Bar"": {
+            ""type"": [
+              ""null"",
+              ""string""
+            ]
+          }
+        },
+        ""required"": [ ""kind"", ""Bar"" ],
+        ""allOf"": [
+          {
+            ""$ref"": ""#/definitions/Animal""
+          }
+        ]
+      },
+      ""Cat"": {
+        ""type"": ""object"",
+        ""properties"": {
+          ""kind"": {
+            ""type"": ""string"",
+            ""enum"": [ ""Cat"" ]
+          },
+          ""Baz"": {
+            ""type"": [
+              ""null"",
+              ""string""
+            ]
+          }
+        },
+        ""required"": [ ""kind"", ""Baz"" ],
+        ""allOf"": [
+          {
+            ""$ref"": ""#/definitions/Animal""
+          }
+        ]
+      },
+      ""Animal"": {
+        ""type"": ""object"",
+        ""discriminator"": ""kind"",
+        ""x-abstract"": true,
+        ""required"": [
+          ""kind""
+        ],
+        ""properties"": {
+          ""Foo"": {
+            ""type"": [
+              ""null"",
+              ""string""
+            ]
+          },
+          ""kind"": {
+            ""type"": ""string""
+          }
+        }
+      }
+    }
+  }
+");
+            var generator = TestHelper.CreateGenerator(schema);
+            var code = generator.GenerateFile();
+            Assert.IsTrue(code.Contains("class Dog : Animal"), "Derived types do not inherit from base type.");
+            Assert.IsTrue(!code.Contains("public enum DogKind"), "Discriminator property is repeated in derived types.");
+            Assert.IsTrue(code.Contains("public Animal Animal"), "Container property type does not match base type.");
+            Assert.IsTrue(code.Contains("[JsonInheritanceAttribute(\"Dog\", typeof(Dog))]"));
+            Assert.IsTrue(code.Contains("[YamlDiscriminator(\"kind\")]"));
+            CompilerTestHelper.CompileFromSource(code);
+        }
     }
 }
