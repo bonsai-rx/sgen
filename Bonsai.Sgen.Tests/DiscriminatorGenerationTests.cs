68494ac57406b06c602943f7ddf822427d69dd10
glopesdev <glopesdev@users.noreply.github.com>
2024-01-18 14:17:17 +0000

    Add regression testing for inline discriminators

--- Bonsai.Sgen.Tests/DiscriminatorGenerationTests.cs
+++ Bonsai.Sgen.Tests/DiscriminatorGenerationTests.cs
@@ -202,5 +202,83 @@ namespace Bonsai.Sgen.Tests
             AssertDiscriminatorAttribute(code, serializerLibraries, "kind");
             CompilerTestHelper.CompileFromSource(code);
         }
+
+        [TestMethod]
+        [DataRow(SerializerLibraries.YamlDotNet)]
+        [DataRow(SerializerLibraries.NewtonsoftJson)]
+        [DataRow(SerializerLibraries.NewtonsoftJson | SerializerLibraries.YamlDotNet)]
+        public async Task GenerateFromOneOfDiscriminatorSchemaProperty_SerializerAnnotationsDeclareKnownTypes(SerializerLibraries serializerLibraries)
+        {
+            var schema = await JsonSchema.FromJsonAsync(@"
+{
+  ""$schema"": ""http://json-schema.org/draft-04/schema#"",
+  ""type"": ""object"",
+  ""title"": ""Container"",
+  ""properties"": {
+    ""Animal"": {
+      ""x-abstract"": true,
+      ""discriminator"": {
+        ""propertyName"": ""kind"",
+        ""mapping"": {
+          ""Dog"": ""#/definitions/Dog"",
+          ""Cat"": ""#/definitions/Cat""
+        }
+      },
+      ""oneOf"": [
+        {
+          ""$ref"": ""#/definitions/Dog""
+        },
+        {
+          ""$ref"": ""#/definitions/Cat""
+        },
+        {
+          ""type"": ""null""
+        }
+      ]
+    }
+  },
+  ""definitions"": {
+    ""Dog"": {
+      ""type"": ""object"",
+      ""properties"": {
+        ""kind"": {
+          ""enum"": [ ""Dog"" ]
+        },
+        ""Bar"": {
+          ""type"": [
+            ""null"",
+            ""string""
+          ]
+        }
+      },
+      ""required"": [ ""kind"", ""Bar"" ]
+    },
+    ""Cat"": {
+      ""type"": ""object"",
+      ""properties"": {
+        ""kind"": {
+          ""enum"": [ ""Cat"" ]
+        },
+        ""Baz"": {
+          ""type"": [
+            ""null"",
+            ""string""
+          ]
+        }
+      },
+      ""required"": [ ""kind"", ""Baz"" ]
+    }
+  }
+}
+");
+            var generator = TestHelper.CreateGenerator(schema, serializerLibraries);
+            var code = generator.GenerateFile();
+            Assert.IsTrue(code.Contains("class Dog : Animal"), "Derived types do not inherit from base type.");
+            Assert.IsTrue(!code.Contains("public enum DogKind"), "Discriminator property is repeated in derived types.");
+            Assert.IsTrue(code.Contains("Animal Animal"), "Container element type does not match base type.");
+            Assert.IsTrue(code.Contains("[JsonInheritanceAttribute(\"Dog\", typeof(Dog))]"));
+            AssertDiscriminatorAttribute(code, serializerLibraries, "kind");
+            CompilerTestHelper.CompileFromSource(code);
+        }
     }
 }
