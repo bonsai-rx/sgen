5fa58d24870896e04780616a4fdeda425bc6393a
glopesdev <glopesdev@users.noreply.github.com>
2024-01-18 14:20:08 +0000

    Support inline oneOf discriminated properties

--- Bonsai.Sgen/JsonSchemaExtensions.cs
+++ Bonsai.Sgen/JsonSchemaExtensions.cs
@@ -15,17 +15,41 @@ namespace Bonsai.Sgen
 
         class DiscriminatorSchemaVisitor : JsonSchemaVisitorBase
         {
-            public DiscriminatorSchemaVisitor(object rootObject)
+            public DiscriminatorSchemaVisitor(JsonSchema rootObject)
             {
                 RootObject = rootObject;
             }
 
-            public object RootObject { get; }
+            public JsonSchema RootObject { get; }
 
             protected override JsonSchema VisitSchema(JsonSchema schema, string path, string typeNameHint)
             {
                 if (schema.DiscriminatorObject != null)
                 {
+                    if (schema is JsonSchemaProperty schemaProperty)
+                    {
+                        if (!RootObject.Definitions.ContainsKey(typeNameHint))
+                        {
+                            var discriminatorSchema = new JsonSchema();
+                            discriminatorSchema.DiscriminatorObject = schemaProperty.DiscriminatorObject;
+                            discriminatorSchema.IsAbstract = schemaProperty.IsAbstract;
+                            foreach (var derivedSchema in schemaProperty.OneOf)
+                            {
+                                if (derivedSchema.IsNullable(SchemaType.JsonSchema))
+                                {
+                                    continue;
+                                }
+
+                                derivedSchema.ActualSchema.AllOf.Add(new JsonSchema { Reference = discriminatorSchema });
+                            }
+                            RootObject.Definitions.Add(typeNameHint, discriminatorSchema);
+                        }
+
+                        schemaProperty.DiscriminatorObject = null;
+                        schemaProperty.IsAbstract = false;
+                        return schema;
+                    }
+
                     foreach (var derivedSchema in schema.GetDerivedSchemas(RootObject).Keys)
                     {
                         foreach (var property in derivedSchema.Properties.Keys.ToList())
