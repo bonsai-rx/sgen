7471b3ec91008990cffba6f16efb4e9688742937
glopesdev <glopesdev@users.noreply.github.com>
2025-03-11 20:28:01 +0000

    Add regression test for simple property default

--- Bonsai.Sgen.Tests/PropertyGenerationTests.cs
+++ Bonsai.Sgen.Tests/PropertyGenerationTests.cs
@@ -35,5 +35,27 @@ namespace Bonsai.Sgen.Tests
             Assert.IsTrue(code.Contains("Required=Newtonsoft.Json.Required.AllowNull"), "Missing property annotation.");
             CompilerTestHelper.CompileFromSource(code);
         }
+
+        [TestMethod]
+        public async Task GenerateFromPropertyDefault_EnsureFieldInitializer()
+        {
+            var schema = await JsonSchema.FromJsonAsync(@"
+{
+    ""$schema"": ""http://json-schema.org/draft-04/schema#"",
+    ""type"": ""object"",
+    ""title"": ""Container"",
+    ""properties"": {
+      ""name"": {
+        ""default"": ""default_name"",
+        ""type"": ""string""
+      }
+    }
+}
+");
+            var generator = TestHelper.CreateGenerator(schema);
+            var code = generator.GenerateFile();
+            Assert.IsTrue(code.Contains("private string _name = \"default_name\""), "Missing field initializer.");
+            CompilerTestHelper.CompileFromSource(code);
+        }
     }
 }
