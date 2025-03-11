d09b029b2f3a9501a7bb32ed31464c947a0870e1
glopesdev <glopesdev@users.noreply.github.com>
2025-03-11 20:23:04 +0000

    Add regression test for required nullable property

---
+++ Bonsai.Sgen.Tests/PropertyGenerationTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NJsonSchema;
+
+namespace Bonsai.Sgen.Tests
+{
+    [TestClass]
+    public class PropertyGenerationTests
+    {
+        [TestMethod]
+        public async Task GenerateFromRequiredNullableProperty_EnsurePropertyAnnotation()
+        {
+            var schema = await JsonSchema.FromJsonAsync(@"
+{
+    ""$schema"": ""http://json-schema.org/draft-04/schema#"",
+    ""type"": ""object"",
+    ""title"": ""Container"",
+    ""properties"": {
+      ""name"": {
+        ""oneOf"": [
+          {
+            ""type"": ""string""
+          },
+          {
+            ""type"": ""null""
+          }
+        ]
+      }
+    },
+    ""required"": [""name""]
+}
+");
+            var generator = TestHelper.CreateGenerator(schema);
+            var code = generator.GenerateFile();
+            Assert.IsTrue(code.Contains("Required=Newtonsoft.Json.Required.AllowNull"), "Missing property annotation.");
+            CompilerTestHelper.CompileFromSource(code);
+        }
+    }
+}
