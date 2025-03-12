d351a194d1e7d8a68cec16a722a96dd00ecdb81d
glopesdev <glopesdev@users.noreply.github.com>
2025-03-12 00:30:46 +0000

    Add regression test for complex property default

--- Bonsai.Sgen.Tests/PropertyGenerationTests.cs
+++ Bonsai.Sgen.Tests/PropertyGenerationTests.cs
@@ -79,5 +79,72 @@ namespace Bonsai.Sgen.Tests
             Assert.IsTrue(code.Contains("_items = new System.Collections.Generic.List<string>();"), "Missing field initializer.");
             CompilerTestHelper.CompileFromSource(code);
         }
+
+        [TestMethod]
+        public async Task GenerateFromComplexPropertyDefault_EnsureFieldInitializer()
+        {
+            var schema = await JsonSchema.FromJsonAsync(@"
+{
+    ""$schema"": ""http://json-schema.org/draft-04/schema#"",
+    ""type"": ""object"",
+    ""title"": ""Container"",
+    ""definitions"": {
+      ""Bar"": {
+        ""properties"": {
+          ""value"": {
+            ""default"": 1,
+            ""type"": ""integer""
+          },
+          ""label"": {
+            ""default"": ""default"",
+            ""type"": ""string""
+          }
+        },
+        ""title"": ""Bar"",
+        ""type"": ""object""
+      },
+      ""Foo"": {
+        ""properties"": {
+          ""foo_label"": {
+            ""default"": ""foo_default_label"",
+            ""type"": ""string""
+          },
+          ""bar_with_default"": {
+            ""allOf"": [{
+              ""$ref"": ""#/definitions/Bar""
+            }],
+            ""default"": {
+              ""value"": 0,
+              ""label"": ""foo_default""
+            }
+          }
+        },
+        ""title"": ""Foo"",
+        ""type"": ""object""
+      }
+    },
+    ""properties"": {
+      ""foo_with_default"": {
+        ""allOf"": [{
+          ""$ref"": ""#/definitions/Foo""
+        }],
+        ""default"": {
+          ""foo_label"": ""foo_default_label"",
+          ""bar_with_default"": {
+            ""label"": ""top_default"",
+            ""value"": 2
+          }
+        }
+      }
+    }
+}
+");
+            var generator = TestHelper.CreateGenerator(schema);
+            var code = generator.GenerateFile();
+            Assert.IsTrue(code.Contains("_barWithDefault.Label = \"foo_default\""));
+            Assert.IsTrue(code.Contains("_fooWithDefault.BarWithDefault.Value = 2"));
+            Assert.IsTrue(code.Contains("_fooWithDefault.BarWithDefault.Label = \"top_default\""));
+            CompilerTestHelper.CompileFromSource(code);
+        }
     }
 }
