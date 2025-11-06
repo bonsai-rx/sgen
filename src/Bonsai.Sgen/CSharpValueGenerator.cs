ba3bb703898b47fec7a2ac2ddfa80a2e64c3e9e9
glopesdev <g.lopes@neurogears.org>
2025-11-06 01:20:16 +0000

    Avoid qualifying generated enum default values (#98)
    * Add regression test for qualified enum name issue
    
    When generating default values for enum fields and the generated code
    contains a type with the same name as the namespace, the resulting
    qualified enum type name will refer to the class and fail resolution.
    
    * Avoid qualifying generated enum default values
    
    This is a workaround to avoid qualifying the generated enum value
    identifier. We know the simple version must never clash in generated
    code since it is used in property type declarations.

---
+++ src/Bonsai.Sgen/CSharpValueGenerator.cs
@@ -0,0 +1,29 @@
+﻿using NJsonSchema;
+using NJsonSchema.CodeGeneration;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace Bonsai.Sgen
+{
+    public class CSharpValueGenerator(CSharpGeneratorSettings settings) : NJsonSchema.CodeGeneration.CSharp.CSharpValueGenerator(settings)
+    {
+        readonly EnumValueGeneratorBase enumValueGenerator = new(settings);
+
+        protected override string GetEnumDefaultValue(JsonSchema schema, JsonSchema actualSchema, string typeNameHint, TypeResolverBase typeResolver)
+        {
+            return enumValueGenerator.GetEnumDefaultValue(schema, actualSchema, typeNameHint, typeResolver);
+        }
+
+        class EnumValueGeneratorBase(CodeGeneratorSettingsBase settings) : ValueGeneratorBase(settings)
+        {
+            public new string GetEnumDefaultValue(JsonSchema schema, JsonSchema actualSchema, string typeNameHint, TypeResolverBase typeResolver)
+            {
+                return base.GetEnumDefaultValue(schema, actualSchema, typeNameHint, typeResolver);
+            }
+
+            public override string GetNumericValue(JsonObjectType type, object value, string format)
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
